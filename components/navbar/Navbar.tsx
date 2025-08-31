cdc20560d7ef31f0ee3b7e6963a85ebc917845e5
Cristian Sanchez <cristian.sanchez@agilesoft.cl>
2025-08-31 18:44:45 -0400

    active links

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import { HomeIcon } from "@primer/octicons-react";
 import Link from "next/link";
 import React from "react";
+import { ActiveLink } from '../active-link/ActiveLink';
 
 const navItems = [
-   { path: "/about", text: "About" },
+   { path: "/about", text: "About"},
    { path: "/pricing", text: "Pricing" },
    { path: "/contact", text: "Contact" },
 ];
@@ -17,9 +18,7 @@ export const Navbar = () => {
          </Link>
          <div className="flex flex-1"></div>
          {navItems.map((navItem) => (
-            <Link key={navItem.path} className="mr-2" href={navItem.path}>
-               {navItem.text}
-            </Link>
+            <ActiveLink key={navItem.path} {...navItem}/>
          ))}
       </nav>
    );
