2e123b91306e12a45526df14ebdc5a2507f5b44e
Cristian Sanchez <cristian.sanchez@agilesoft.cl>
2025-08-31 18:02:56 -0400

    next link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import { HomeIcon } from "@primer/octicons-react";
+import Link from "next/link";
+import React from "react";
+
+const navItems = [
+   { path: "/about", text: "About" },
+   { path: "/pricing", text: "Pricing" },
+   { path: "/contact", text: "Contact" },
+];
 
 export const Navbar = () => {
-  return (
-    <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
-        <span>Home</span>
-        <div className='flex flex-1'></div>
-        <a className='mr-2' href="/about">About</a>
-        <a className='mr-2' href="/contact">Contact</a>
-        <a className='mr-2' href="/pricing">Price</a>
-    </nav>
-  )
-}
+   return (
+      <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
+         <Link href="/" className="flex items-center">
+            <HomeIcon className="mr-2" />
+            <span>Home</span>
+         </Link>
+         <div className="flex flex-1"></div>
+         {navItems.map((navItem) => (
+            <Link key={navItem.path} className="mr-2" href={navItem.path}>
+               {navItem.text}
+            </Link>
+         ))}
+      </nav>
+   );
+};
