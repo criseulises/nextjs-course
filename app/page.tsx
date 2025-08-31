69efce286cbb7281d93c15bde6791a2c0525d89d
Cristian Sanchez <cristian.sanchez@agilesoft.cl>
2025-08-31 19:01:38 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid items-center justify-items-center p-8 pb-20 gap-16">
-      <span className="text-5xl">Hola Mundo</span>
-    </div>
-  );
+export default function HomePage() {
+   return (
+      <main className="flex flex-col items-center p-24">
+         <div className="font-sans grid items-center justify-items-center p-8 pb-20 gap-16">
+            <span className="text-5xl">Hola Mundo</span>
+         </div>
+
+        <Link href={"/about"}>About Page</Link>
+      </main>
+   );
 }
